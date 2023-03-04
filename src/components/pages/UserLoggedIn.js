43a349d8506ed1ac10d71ecddec360d43307e297
Philip Wagih <philipwagih9@gmail.com>
2023-03-04 23:28:41 +0200

    added upload button

--- src/components/pages/UserLoggedIn.js
+++ src/components/pages/UserLoggedIn.js
@@ -5,14 +5,35 @@ import '../UserLoggedIn.css'
 // import { ChangeEvent, useState, use } from 'react';
 
 
-function UserLoggedIn() {
+const UserLoggedIn = props => {
+	// Create a reference to the hidden file input element
+	const hiddenFileInput = React.useRef(null);
+  
+	// Programatically click the hidden file input element
+	// when the Button component is clicked
+	const handleClick = event => {
+	  hiddenFileInput.current.click();
+	};
+	
+	// Call a function (passed as a prop from the parent component)
+	// to handle the user-selected file 
+	const handleChange = event => {
+	  const fileUploaded = event.target.files[0];
+	  props.handleFile(fileUploaded);
+	};
 	return(
 		<div className='land-hero-container'>
 			<h1>SCAN PRODUCT TO BEGIN</h1>
 			<div className='hero-btns'>
-				<Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+				<Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large' onClick={handleClick}>
 					UPLOAD QR CODE
 				</Button>
+				<input
+					type="file"
+					accept="image/*"
+					ref={hiddenFileInput}
+					onChange={handleChange}
+					style={{display: 'none'}} />
 			</div>
 		</div>
 	);
